d74bc68c93ccf75837ffbe8fa7600e2a1a9b02dc
Joshua Rudesill <joshrudesill@gmail.com>
2023-07-15 15:46:10 +0200

    adding binaries

---
+++ public/pkg/recad_wasm.d.ts
@@ -0,0 +1,44 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} left
+* @param {number} right
+* @returns {number}
+*/
+export function add(left: number, right: number): number;
+/**
+* @param {number} cursor
+* @param {number} stage
+* @param {number} inverse_z
+* @returns {number}
+*/
+export function derive_actual_pos(cursor: number, stage: number, inverse_z: number): number;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly add: (a: number, b: number) => number;
+  readonly derive_actual_pos: (a: number, b: number, c: number) => number;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ public/pkg/recad_wasm_bg.wasm.d.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function add(a: number, b: number): number;
+export function derive_actual_pos(a: number, b: number, c: number): number;
